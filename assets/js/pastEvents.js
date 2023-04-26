20e49833247b5c6d0eb6bb80ff61120d1716fa0f
Guillermo Duque <guille980627@gmail.com>
2023-04-26 17:21:25 -0500

    swiper function added

--- assets/js/pastEvents.js
+++ assets/js/pastEvents.js
@@ -40,6 +40,20 @@ function loadItems(allEvents) {
   }
 }
 
+//control del swiper
+let swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  grabCursor: 'true',
+  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+  breakpoints: {
+      540: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 40 },
+      820: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 40 },
+      1240: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 40 },
+  }
+});
+
 async function fetchApi() {
   try {
     let url = "https://pro-talento.up.railway.app/api/amazing/?time=past";
