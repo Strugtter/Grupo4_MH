f79b758e7e504e4cb1a125d4e29c5e26a4a4fe63
Guillermo Duque <guille980627@gmail.com>
2023-04-27 17:23:13 -0500

    events stats working

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,14 +1,15 @@
 let categorysUp = document.getElementById('tableBody');
 let url = 'https://pro-talento.up.railway.app/api/amazing?time=upcoming';
+let api = "";
 
 async function loadStatsUp(url) {
     let response = await fetch(url);
     response = await response.json()
     response = response.response;
-    console.log(response);
+    // console.log(response);
     let categorysArray = [...new Set(response.map(event => event.category))];
     let filas = categorysUp.rows;
-    console.log(filas);
+    // console.log(filas);
 
     categorysArray.forEach(category => {
         let eventosCategorys = response.filter(event => event.category == category);
@@ -17,9 +18,7 @@ async function loadStatsUp(url) {
         let capacityTotal = eventosCategorys.map(event => event.capacity).reduce((acumulado, actual) => acumulado + actual);
         
         let percentOfAsistence = (EstimateTotal / capacityTotal)*100
-        
-        
-        
+    
         let fila = categorysUp.insertRow(filas[2].rowIndex);
         let celda1 = fila.insertCell();
         let celda2 = fila.insertCell();
@@ -32,7 +31,6 @@ async function loadStatsUp(url) {
 
 const urlData = 'https://pro-talento.up.railway.app/api/amazing?time=past'
 
-
 async function statsPast (urlData) {
     const statsPast = document.getElementById("pasados");
     let response = await fetch(urlData);
@@ -40,14 +38,14 @@ async function statsPast (urlData) {
     response = response.response
     let allEvents = response;
     let categorias = [...new Set(allEvents.map(category => category.category))]//modifico el arreglo solo a categorias para mostrarlo en la tabla
-    console.log(categorias)
+    // console.log(categorias)
     categorias.forEach((category)=> {
         let categoriasAgrupadas = allEvents.filter( each => each.category == category);
         let ingresosPorCategoria = categoriasAgrupadas.map(each => each.price*each.assistance).reduce((acumulador,valor)=>acumulador+valor);
         let totalAsitencia = categoriasAgrupadas.map(each => each.assistance*100).reduce((acumulador, valor)=>acumulador+valor);
         let totalCapacidad = categoriasAgrupadas.map(each => each.capacity).reduce((acumulador, valor)=>acumulador+valor);
         let porcentajePorCategoria = totalAsitencia / totalCapacidad;
-        console.log(porcentajePorCategoria)
+        // console.log(porcentajePorCategoria)
         statsPast.innerHTML += `<tr>
         <td>${category}</td>
         <td>${ingresosPorCategoria.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
@@ -57,3 +55,22 @@ async function statsPast (urlData) {
     }) 
 }
 statsPast (urlData)
+
+async function EventsStats(api) {
+    let response = await fetch(api).then(response => response.json());
+        response = response.response;
+        const hatendance = response.reduce((ac, actual) => (ac.assistance/ac.capacity > actual.assistance/actual.capacity) ? ac : actual);
+        const latendance = response.reduce((ac, actual) => (ac.assistance/ac.capacity < actual.assistance/actual.capacity) ? ac : actual);
+        api = "https://pro-talento.up.railway.app/api/amazing";
+        response = await fetch(api).then(response => response.json());
+        response = response.response;
+        const lcapacity = response.reduce((maximo, evento) => (maximo.capacity > evento.capacity) ? maximo : evento);
+        // console.log(lcapacity.capacity.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 }));
+        let content = document.getElementById('main-view');
+        content.innerHTML = `
+        <td>${hatendance.name}</td>
+        <td>${latendance.name}</td>
+        <td>${lcapacity.name}</td>
+        `;    
+} EventsStats(urlData)
+
